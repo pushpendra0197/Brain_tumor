117e26fbc87f52350e34f7459f5a46c58033b684
Pushpendra Dhamanya <162273949+pushpendra0197@users.noreply.github.com>
2025-05-25 14:08:54 +0530

    Update brain_tumor.py

--- brain_tumor.py
+++ brain_tumor.py
@@ -27,8 +27,8 @@ upload=st.file_uploader("Upload Image of Mri",["jpg", "jpeg", "png"])
 button=st.button("Predict")
 if button:
     if upload is not None:
-      image=Image.open(upload)
-      resize_image=image.resize(256,256)
+      image=cv2.imread(upload)
+      resize_image=(cv2.resize(image,dsize=(256,256),interpolation=cv2.INTER_AREA)
       final_image=np.expand_dims(resize_image,axis=0)
       Model= tensorflow.keras.models.load_model(r"Braintumor.keras")
       prediction=Model.predict(final_image)
