a8d8677e2464ffdaba90607d0596f09423ae154f
pushpendra <pushpendradhamanya523@gmail.com>
2025-05-25 13:16:30 +0530

    push

---
+++ brain_tumor.py
@@ -0,0 +1,37 @@
+import numpy  as np
+import pandas as pd
+import tensorflow
+import streamlit as st
+from tensorflow.python.keras.models import load_model
+import matplotlib.pyplot as plt
+from PIL import Image
+import base64
+import cv2 
+st.set_page_config(page_title="Brain Tumor Detector",page_icon=r"images.png")
+path=(r"image.png")
+with open(path,"rb") as file:
+    image_back=base64.b64encode(file.read()).decode()
+page_element=f"""
+<style>
+[data-testid="stAppViewContainer"]
+{{
+background-image:url("data:image;base64,{image_back}");
+background-size:2000px 1000px;
+background-repeat:no-repeat;
+background-position:center;
+}}
+<style>
+"""
+st.markdown(page_element,unsafe_allow_html=True)
+upload=st.file_uploader("Upload Image of Mri",["jpg", "jpeg", "png"])
+button=st.button("Predict")
+if button:
+    if upload is not None:
+      image=Image.open(upload)
+      resize_image=image.resize(size=(256,256))
+      final_image=np.expand_dims(resize_image,axis=0)
+      Model= tensorflow.keras.models.load_model(r"Braintumor.keras")
+      prediction=Model.predict(final_image)
+      class_name=['Glioma', 'Healthy', 'Meningioma', 'Pituitary']
+      output=class_name[np.argmax(prediction)]
+      st.header(output,divider="rainbow")
